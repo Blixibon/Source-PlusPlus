dcebcbf7cb43fd0cbe8a7c2bdcb8c065bb0c0f40
Petercov <petercov@outlook.com>
2019-07-16 17:12:25 -0400

    Lazuul voicemenu

--- game/mod_lazuul/run_server.bat
+++ game/mod_lazuul/run_server.bat
@@ -2,4 +2,4 @@
 
 set GameDir=%~dp0
 set GameDir=%GameDir:~0,-1%
-"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Dedicated Server\srcds.exe" -game %GameDir% -dev -console -maxplayers 24 -insecure
+"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Dedicated Server\srcds.exe" -game %GameDir% -dev -console -maxplayers 32 -insecure
