2e54bdb5100ea2d4abbceeb80aa0251becea0e9a
Peter Covington <petercov@outlook.com>
2020-01-06 16:02:24 -0500

    Cleanup

--- src/creategameshl2.bat
+++
@@ -1,1 +0,0 @@
-devtools\bin\vpc.exe /2013 /hl2 /hl2mp /episodic +game +shaders /mksln hl2games.sln
--- src/creategameshl2deferred.bat
+++
@@ -1,1 +0,0 @@
-devtools\bin\vpc.exe /2013 /define:deferred 1 /hl2 +game +shaders /mksln hl2def.sln
